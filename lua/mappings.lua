539fa6bc1a2f72a886b08e3275ed923bbd33b52a
Florian Hamal <florian.hamal@bytepoets.com>
2026-02-18 14:31:43 +0100

    added git revert keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -20,6 +20,17 @@ map("n", "<leader>gr", "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = "Git undo s
 map("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", { desc = "Git view diff" })
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 
+-- Reset (Revert) Hunk: Only revert the block under the cursor
+map("n", "<leader>rh", "<cmd>Gitsigns reset_hunk<cr>", { desc = "Git reset hunk" })
+
+-- Reset (Revert) Buffer: Revert the ENTIRE file to its last commit state
+map("n", "<leader>rb", "<cmd>Gitsigns reset_buffer<cr>", { desc = "Git reset buffer" })
+
+-- Visual Mode: Revert only the lines you have selected
+map("v", "<leader>rh", function()
+  require("gitsigns").reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "Git reset selected hunk" })
+
 -- Quick Commit (Opens a prompt for the message)
 map("n", "<leader>gc", function()
   local msg = vim.fn.input("Commit Message: ")
