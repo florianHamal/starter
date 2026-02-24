5e51b10b76ff69d538d3d6f44e62ba149dee898d
Florian Hamal <florian.hamal@bytepoets.com>
2026-02-24 14:31:46 +0100

    added dart to lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local lspconfig = require "lspconfig"
 local nvc = require "nvchad.configs.lspconfig"
 
-local servers = { "html", "cssls","jdtls" }
+local servers = { "html", "cssls","jdtls", "dartls" }
 
 vim.lsp.enable(servers)
 
