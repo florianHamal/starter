06d660d12408567d02df4e8f2712aa3baca7ea1b
Florian Hamal <florian.hamal@bytepoets.com>
2026-02-26 10:16:16 +0100

    fixed dart lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,5 +3,13 @@ local nvc = require "nvchad.configs.lspconfig"
 
 local servers = { "html", "cssls","jdtls" }
 
+local fvm_dart = vim.fn.expand("~/fvm/default/bin/dart")
+
+lspconfig.dartls.setup {
+  cmd = { fvm_dart, "language-server", "--protocol=lsp" },
+  rootPatterns = { "pubspec.yaml", ".dart_tool" },
+  filetypes = { "dart" },
+}
+
 vim.lsp.enable(servers)
 
