7361a5b3d12edf0b6ec4565c94519231c71643ce
Florian Hamal <florian.hamal@bytepoets.com>
2026-03-04 22:08:56 +0100

    disabled anoying blocking popup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,12 +82,17 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      -- add any options here
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
+        },
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
         },
       },
     },
