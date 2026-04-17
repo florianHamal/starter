3f3ba1af33508648bdf0958ca15512a3e0b2f348
Florian Hamal <florian.hamal@bytepoets.com>
2026-04-17 12:00:20 +0200

    added fish support

--- lua/options.lua
+++ lua/options.lua
@@ -7,5 +7,14 @@ vim.opt.linebreak = false
 vim.opt.sidescroll = 1
 vim.opt.sidescrolloff = 5
 
+-- force Neovim to use fish for :! and terminal
+local fish = vim.fn.exepath "fish"
+if fish ~= "" then
+  vim.opt.shell = fish
+  vim.opt.shellcmdflag = "-c"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
