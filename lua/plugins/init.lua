fa2ebcac95e9c54ee55c779b5434d361eaa85249
Florian Hamal <florian.hamal@bytepoets.com>
2026-06-24 15:56:20 +0200

    added db integration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -221,6 +221,20 @@ return {
     end,
   },
   {
+    "tpope/vim-dadbod",
+    cmd = { "DB" },
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = { "tpope/vim-dadbod" },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown" },
     cmd = { "RenderMarkdown" },
